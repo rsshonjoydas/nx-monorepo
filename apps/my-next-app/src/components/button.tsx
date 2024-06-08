2bf8affd8ab6c8bce2b40d4d1039a1699d9c7014
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2024-06-08 19:42:21 +0600

    ci: husky setup

--- apps/my-next-app/src/components/button.tsx
+++ apps/my-next-app/src/components/button.tsx
@@ -1,7 +1,7 @@
 export const GlobalButton = () => (
   <button
     type='button'
-    className='hover:bg-primary/90 bg-primary text-primary-foreground'
+    className='bg-primary text-primary-foreground hover:bg-primary/90'
   >
     component Button
   </button>
